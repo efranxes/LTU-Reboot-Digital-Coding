da1e06f8ae7f94ec3521f58279b46c7fe1ece427
Ella <93593284+efranxes@users.noreply.github.com>
2021-11-15 13:37:05 +0000

    Update main.js
    Log 'Hello'
    Set variables and log on console

---
+++ module-3/task/start/scripts/main.js
@@ -0,0 +1,31 @@
+console.log('Hello');
+
+//Single line comment
+
+/*--- 
+Multi
+line
+comment
+---*/
+
+/*--
+
+var CAN be redeclared or overwritten
+
+let CAN'T be redeclared but CAN be overwritten
+
+const CAN'T be redeclared or overwritten
+
+--*/
+
+let productName = 'chicken';
+let price = '£4.20';
+let quantity = '1';
+let inStock = true;
+let discountAmount = 0;
+
+console.log(productName);
+console.log(price);
+console.log(quantity);
+console.log(inStock);
+console.log(discountAmount);

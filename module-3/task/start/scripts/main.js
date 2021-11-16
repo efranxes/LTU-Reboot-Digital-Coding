daee742d0df6905ca2fed723a0b872ad7e7ff799
Ella <93593284+efranxes@users.noreply.github.com>
2021-11-16 21:03:29 +0000

    Update main.js
    Add TASK 4 - TASK 7

--- module-3/task/start/scripts/main.js
+++ module-3/task/start/scripts/main.js
@@ -1,5 +1,7 @@
 console.log('Hello');
 
+//TASK 2
+
 //Single line comment
 
 /*--- 
@@ -8,24 +10,72 @@ line
 comment
 ---*/
 
-/*--
 
-var CAN be redeclared or overwritten
-
-let CAN'T be redeclared but CAN be overwritten
-
-const CAN'T be redeclared or overwritten
-
---*/
+//TASK 3
 
 let productName = 'chicken';
-let price = '£4.20';
-let quantity = '1';
+let price = '4.20';
+let quantity = '5';
 let inStock = true;
-let discountAmount = 0;
+let discountAmount = 2;
 
-console.log(productName);
+/*-- console.log(productName);
 console.log(price);
 console.log(quantity);
 console.log(inStock);
 console.log(discountAmount);
+--*/
+
+
+//TASK 4
+
+function showProductName(){
+    console.log(productName);
+}
+
+showProductName();
+
+
+//TASK 5
+
+function totalPrice(productPrice=0,productQuantity=0){
+    productPrice = parseFloat(productPrice);
+    productQuantity = parseFloat(productQuantity);
+    let totalProductPrice = productPrice * productQuantity;
+    console.log(totalProductPrice);
+}
+
+totalPrice(price,quantity);
+
+
+//TASK 6
+
+function squareNumber(number=0){
+    let sum = number * number;
+    return sum;
+}
+
+var square = num => num * num;
+
+let squaredNumber = squareNumber(3);
+console.log(squaredNumber);
+
+let squared = square(3);
+console.log(squared);
+
+
+//TASK 7
+
+function productDiscount(){
+    let sum;
+    if(quantity > 1){
+        sum = price * quantity;
+        let discount = discountAmount * quantity;
+        sum = sum - discount;
+    } else {
+        sum = price * quantity;
+    }
+    console.log(sum);
+}
+
+productDiscount();
